f0140e437b3c3cd19b6009133093fe9a18b39a84
yukimura <38983738+candytuftt@users.noreply.github.com>
2018-05-09 22:40:33 +0900

    Add files via upload

---
+++ channel.js
@@ -0,0 +1,54 @@
+const Discord = require( 'discord.js' );
+const fs = require( 'fs' );
+const client = new Discord.Client();
+const token = "NDM3MjEwMzY3ODE5NTEzODU3.DcM10g.qlx8Ip4JUk_P8jtvoEjLazWZdQ0"; // トークンに置き換え
+ 
+ 
+ 
+client.on("ready", () => {
+  console.log("bot is start");
+
+
+client.user.setActivity('discord.js');
+});
+ 
+client.on("message", (message) =>
+{
+
+  let arg = message.content.split( /\s+/ );
+  const cmd = arg.shift();
+  const ch_name = arg[0];
+ 
+  if ( cmd === '!ch' )
+  {
+
+    if ( !message.guild.channels.exists( 'name', ch_name ) )
+    {
+      message.guild.createChannel( ch_name, 'text' )
+        .then( (ch) => {
+          ch.send( message.member.displayName + 'が' + ch_name + 'を作成しました' );
+        })
+        .catch( (err) => { console.log( err ); } );
+    }
+  }
+  if ( cmd === '!dch' )
+  {
+    let channel = message.guild.channels.find( 'name', ch_name );
+ {
+
+      {
+        channel.delete()
+          .then( (ch) => {
+            if ( ch.id !== message.channel.id ) {
+              message.channel.send( message.member.displayName + 'が' + ch_name + 'を削除しました' );
+            }
+          })
+          .catch( (err) => { console.log( err ); } );
+      }
+    }
+  }
+});
+
+
+
+client.login(token);
